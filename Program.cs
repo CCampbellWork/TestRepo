62c6312d64eaf89ec3299e56079d19ffe4d34f7d
CCampbellWork <88452964+CCampbellWork@users.noreply.github.com>
2021-08-04 10:37:31 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Super_Awesome_Brogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Change appearance of console
+            Console.Title = "LochnessConsole";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+
+            Console.WriteLine("Hello, what's your name?");
+
+            Console.ReadLine(); 
+            
+            Console.WriteLine("Okay, jerk.");
+            Console.WriteLine("What is your favorite color?");
+
+            Console.ReadLine();
+            
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Wow, what a crappy colour.");
+
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
